425ade41a71bd18a81374e0e1ccf61ead97fca82
magnu <magnus.s.larsen@hotmail.com>
2023-02-07 14:00:44 +0100

    I gang med 3.5

---
+++ Dag3_Opgave3.2_Medarbejder_Mekaniker_Værkføre_synsmand/MedarbejderDictionary.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dag3_Opgave3._2_Medarbejder_Mekaniker_Værkføre_synsmand
+{
+    internal class MedarbejderDictionary
+    {
+    }
+}
