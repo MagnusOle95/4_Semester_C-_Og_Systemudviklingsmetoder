5909b95cf22aee2eb6481f0f19301c71ecade6d2
magnu <magnus.s.larsen@hotmail.com>
2023-02-09 12:40:58 +0100

    Klar til opgave 4

---
+++ Dag4_Opgave4_Opg3_Med_Andet_end_Subscriber_Patten/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dag4_Opgave4_Opg3_Med_Andet_end_Subscriber_Patten
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Dag4_Opgave4_Opg3_Med_Message_Filtering_Pattern/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dag4_Opgave4_Opg3_Med_Message_Filtering_Pattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
