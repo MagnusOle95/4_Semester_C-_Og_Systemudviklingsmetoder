96d2355d47ef1397d3d551089953761195f3206b
magnu <magnus.s.larsen@hotmail.com>
2023-02-23 14:29:52 +0100

    Færdig med opgave 1 systemintegration.

---
+++ Dag8_Opgave1_xml_Splitter/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Dag8_Opgave1_xml_Splitter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Opretter kør. 
+            //MessageQueue.Create(@".\Private$\AirportCheckInOutput");
+            //MessageQueue.Create(@".\Private$\LuggageQueue");
+            //MessageQueue.Create(@".\Private$\PassengerQueue");
+
+
+            //Forbinder til køerne. 
+            MessageQueue messageQueue = new MessageQueue(@".\Private$\AirportCheckInOutput");
+            MessageQueue Luggagequeue = new MessageQueue(@".\Private$\LuggageQueue");
+            MessageQueue PassengerQueue = new MessageQueue(@".\Private$\PassengerQueue");
+
+
+
+            //Læser og Sender xml fil til checkin Kø
+            XElement CheckInFile = XElement.Load(@"CheckedInPassenger.xml");
+            //Console.WriteLine(CheckInFile);
+            string label = "Label";
+
+            messageQueue.Send(CheckInFile, label);
+
+            //opretter router. 
+            Router_Splitter Router_Splitter = new Router_Splitter(messageQueue,Luggagequeue,PassengerQueue);
+
+            Console.ReadLine();
+
+        }
+            
+    }
+}
---
+++ Dag8_Opgave1_xml_Splitter/Router_Splitter.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Messaging;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Dag8_Opgave1_xml_Splitter
+{
+    internal class Router_Splitter
+    {
+        protected MessageQueue inQueue;
+        protected MessageQueue passengerQueue;
+        protected MessageQueue LuggageQueue;
+        public Router_Splitter(MessageQueue inQueue, MessageQueue luggageQueue, MessageQueue PassengerQueue) 
+        {
+            inQueue.ReceiveCompleted += new ReceiveCompletedEventHandler(OnMessage);
+            inQueue.BeginReceive();
+            string label = inQueue.Label;
+            this.passengerQueue = PassengerQueue;
+            this.LuggageQueue = luggageQueue;
+        }
+
+        private void OnMessage(Object source, ReceiveCompletedEventArgs asyncResult)
+        {
+            //Henter fra køen. 
+            MessageQueue mq = (MessageQueue)source;
+            Message message = mq.EndReceive(asyncResult.AsyncResult);
+
+            //Parse stringen fra køen til en xml fil igen. 
+            StreamReader reader = new StreamReader(message.BodyStream);
+            XElement body = XElement.Parse(reader.ReadToEnd());
+
+            //Laver variabel af Passenger, og sender til kø. 
+            var passenger = body.Element("Passenger");
+            Console.WriteLine(passenger);
+            passengerQueue.Send(passenger, passenger.Element("FirstName").Value);
+
+            //Laver Variabler af luggage og sender til køen. 
+            var luggages = body.Elements("Luggage");
+            Console.WriteLine(luggages);
+            
+            foreach (var l in luggages)
+            {
+                LuggageQueue.Send(l, "Owner: " + passenger.Element("FirstName").Value);
+            }
+
+            //Printer elemeter ud fra listen.
+            
+            string reservationsNummer = passenger.Element("ReservationNumber").Value;
+            string firstName = passenger.Element("FirstName").Value;
+            string lastName = passenger.Element("LastName").Value;
+            Console.WriteLine(reservationsNummer);
+            Console.WriteLine(firstName);
+            Console.WriteLine(lastName);
+            
+
+            //Begynder ny læsning fra køen. 
+            mq.BeginReceive();
+        }
+    }
+}
