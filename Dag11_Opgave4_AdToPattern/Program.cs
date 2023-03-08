c709c6ec26dd053e8eec831ec3e5bfcbb5767c1b
magnu <magnus.s.larsen@hotmail.com>
2023-03-08 17:08:27 +0100

    Opgave4 eller 5 fra dag 8 eller 9 i si

---
+++ Dag11_Opgave4_AdToPattern/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Dag11_Opgave4_AdToPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Læser og Sender xml fil til checkin Kø
+            XElement CheckInFile = XElement.Load(@"CheckedInPassenger.xml");
+            Console.WriteLine(CheckInFile.ToString());
+
+            //Opretter objekt af read total luggage. 
+            ReadLuggageWeight read = new ReadLuggageWeight();
+
+            //Køre metoden til at read total luggage. 
+            XElement nyXml = read.getTotalWeight(CheckInFile);
+
+            //Udskriver afsluttende resultat
+            Console.WriteLine();
+            Console.WriteLine(nyXml.ToString());
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Dag11_Opgave4_AdToPattern/ReadLuggageWeight.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Dag11_Opgave4_AdToPattern
+{
+    public class ReadLuggageWeight
+    {
+        public ReadLuggageWeight() { }
+
+        public XElement getTotalWeight(XElement xmlFile)
+        {
+            //Finder den samlede vægt på bagage.
+            double totalWeight = 0;
+            IEnumerable<XElement> luggage = xmlFile.Elements("Luggage");
+            foreach(XElement ele in luggage) 
+            {
+                String s = ele.Element("Weight").Value;
+                double total = double.Parse(s,CultureInfo.InvariantCulture);
+                totalWeight += total;
+            }
+
+            //Tilføjer den til eksisterende xml under passenger fil og retunere den. 
+            XElement tempXE = xmlFile.Element("Passenger");
+            tempXE.Add(new XElement("totalLuggageWeight", totalWeight));
+            return xmlFile;
+        }
+    }
+}
