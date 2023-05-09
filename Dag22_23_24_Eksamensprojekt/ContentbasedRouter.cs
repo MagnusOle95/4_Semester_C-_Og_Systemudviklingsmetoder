5659b2e3f69058a0e473147ad41d8ce6247a65a4
magnu <magnus.s.larsen@hotmail.com>
2023-05-09 14:39:16 +0200

    Projekt opgave.

---
+++ Dag22_23_24_Eksamensprojekt/ContentbasedRouter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Dag22_23_24_Eksamensprojekt
+{
+    internal class ContentbasedRouter
+    {
+        protected MessageQueue inQueue;
+        protected MessageQueue outQueue_LMS;
+        protected MessageQueue outQueue_PAYS;
+        public ContentbasedRouter(MessageQueue inQueue, MessageQueue outQueue_LMS, MessageQueue outQueue_PAYS)
+        {
+            this.inQueue = inQueue;
+            this.outQueue_LMS = outQueue_LMS;
+            this.outQueue_PAYS = outQueue_PAYS;
+            inQueue.ReceiveCompleted += new ReceiveCompletedEventHandler(OnMessage);
+            inQueue.BeginReceive();
+        }
+        private void OnMessage(Object source, ReceiveCompletedEventArgs asyncResult)
+        {
+            MessageQueue mq = (MessageQueue)source;
+            Message message = mq.EndReceive(asyncResult.AsyncResult);
+
+            //Parse stringen fra køen til en xml fil igen. 
+            StreamReader reader = new StreamReader(message.BodyStream);
+            XElement body = XElement.Parse(reader.ReadToEnd());
+
+
+            //Henter Systemtypen fra XML beskeden ved hjælp af LINQ.
+            string systemType = body.Element("SystemType").Value;
+
+            //Tjekker beskedens systemtype. og sender til kø. 
+            if (systemType == "CheckIn")
+                outQueue_LMS.Send(message);
+            else if (systemType == "Payment")
+                outQueue_PAYS.Send(message);
+
+            mq.BeginReceive();
+        }
+        
+    }
+}
---
+++ Dag22_23_24_Eksamensprojekt/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Dag22_23_24_Eksamensprojekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //////Opretter kør.
+            //MessageQueue.Create(@".\Private$\From_CRead_IDM_To_Router");
+            //MessageQueue.Create(@".\Private$\From_Router_To_LMS");
+            //MessageQueue.Create(@".\Private$\From_Router_To_PAYS");
+
+            //Forbinder til køerne. 
+            MessageQueue From_CRead_IDM_To_Router = new MessageQueue(@".\Private$\From_CRead_IDM_To_Router");
+            MessageQueue From_Router_To_LMS = new MessageQueue(@".\Private$\From_Router_To_LMS");
+            MessageQueue From_Router_To_PAYS = new MessageQueue(@".\Private$\From_Router_To_PAYS");
+
+            //Opretter router. 
+            ContentbasedRouter router = new ContentbasedRouter(From_CRead_IDM_To_Router, From_Router_To_LMS, From_Router_To_PAYS);
+
+            //Henter XML fil ind i systemet. 
+            XmlDocument IDMCheckIn = new XmlDocument();
+            IDMCheckIn.Load("C:\\C#\\4_Semester_C++++_Og_Systemudviklingsmetoder\\Dag22_23_24_Eksamensprojekt\\IDMChechin.xml");
+
+            XmlDocument IDMPayInfo = new XmlDocument();
+            IDMPayInfo.Load("C:\\C#\\4_Semester_C++++_Og_Systemudviklingsmetoder\\Dag22_23_24_Eksamensprojekt\\IDMPayinfo.xml");
+
+            //Nu sender jeg dem til routern. 
+            From_CRead_IDM_To_Router.Send(IDMCheckIn);
+            //From_CRead_IDM_To_Router.Send(IDMPayInfo);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
