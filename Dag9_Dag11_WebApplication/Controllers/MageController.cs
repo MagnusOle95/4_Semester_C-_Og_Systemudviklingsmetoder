f4cdd74bce021d72fcdd90a240cbab11e9153aa3
magnu <magnus.s.larsen@hotmail.com>
2023-05-24 15:28:36 +0200

    Skal videre med API.

--- Dag9_Dag11_WebApplication/Controllers/MageController.cs
+++ Dag9_Dag11_WebApplication/Controllers/MageController.cs
@@ -28,17 +28,21 @@ namespace Dag9_Dag11_WebApplication.Controllers
         {
             int id = int.Parse(formCollection["ID"]);
             Mage tempMage = bll.GetMage(id);
-            ViewBag.Name = tempMage.Name;
-            ViewBag.IsDark = tempMage.IsDark;
-            return View("MageView");
+            ModelMage mm = new ModelMage();
+            mm.ID = id;
+            mm.Name = tempMage.Name;
+            mm.IsDark = tempMage.IsDark;
+            return View("MageView",mm);
         }
 
         public ActionResult GetMageURL(int id)
         {
             Mage tempMage = bll.GetMage(id);
-            ViewBag.Name = tempMage.Name;
-            ViewBag.IsDark = tempMage.IsDark;
-            return View("MageView");
+            ModelMage mm = new ModelMage();
+            mm.ID = id;
+            mm.Name = tempMage.Name;
+            mm.IsDark = tempMage.IsDark;
+            return View("MageView",mm);
         }
 
         public ActionResult updateMage(IFormCollection formCollection)
@@ -52,10 +56,43 @@ namespace Dag9_Dag11_WebApplication.Controllers
             return View("MageView");
         }
 
-        [HttpGet]
         public ActionResult CreateMageView()
         {
             return View();
+        }
+
+        public ActionResult CreateMage(IFormCollection formCollection) 
+        {
+            string Name = formCollection["Name"];
+            string NameNoSpace = Name.Replace(" ", "");
+            bool IsDark = Boolean.Parse(formCollection["IsDark"]);
+            Mage m = new Mage(NameNoSpace, IsDark);
+            bll.addMage(m);
+            return View("CreateMageView");
+        }
+
+        public ActionResult DeleteMageView()
+        {
+            ViewBag.Magelist = bll.getMages();
+            return View();
+        }
+
+        public ActionResult ShwoMageInDeleteMage(string selectedItem)
+        {
+            string s = selectedItem;
+            string[] sArray = s.Split(' ');
+            int mageID = Int32.Parse(sArray[2]);
+            Mage tempMage = bll.GetMage(mageID);
+            return Json(new { success = true, name = tempMage.Name, isDark = tempMage.IsDark, mageId = tempMage.MageId });
+        }
+
+        public ActionResult DeleteMage(IFormCollection formCollection)
+        {
+            int id = int.Parse(formCollection["ID"]);
+            Mage tempMage = bll.GetMage(id);
+            bll.deleteMage(tempMage);
+            ViewBag.Magelist = bll.getMages();
+            return View("DeleteMageView");
         }
 
 
---
+++ Dag9_Dag11_WebApplication/Models/ModelMage.cs
@@ -0,0 +1,11 @@
+﻿namespace Dag9_Dag11_WebApplication.Models
+{
+    public class ModelMage
+    {
+        public int ID { get; set; }
+        public String Name { get; set; }
+        public bool IsDark{get; set;}
+
+    }
+
+}
